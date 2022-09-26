40d8dd056199a8093d808dae1c65a9513ae43294
katerinabc <katerina@netnigma.com>
2022-09-26 09:14:43 +0100

    soc calculation

--- pulse_survey.py
+++ pulse_survey.py
@@ -13,7 +13,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
-def soc(df):
+def soc(df, minscale, maxscale):
     """
     input the data export from blocksurvey
 
@@ -30,9 +30,12 @@ def soc(df):
 
     indi_SoC = (df['Soc_ec1_exp'] + df['Soc_ec2_exp'])/2
 
-    #array = indi_SoC.values.reshape(-1,1)
     scaler = preprocessing.MinMaxScaler(feature_range=(0, 100)) #normalize scores between 0 and 100
-    indi_SoC_n =scaler.fit_transform(indi_SoC.values.reshape(-1,1)) # reshape array from 2d to 1d, and normalize
+    # make sure the default range is from 1 to 5 like the scale
+    df_default = np.array([minscale, maxscale])
+    df_def = scaler.fit_transform(df_default.reshape(-1,1)) 
+    
+    indi_SoC_n = scaler.transform(indi_SoC.values.reshape(-1,1)) # reshape array from 2d to 1d, and normalize
     
     d['min_SoC']  = np.nanmin(indi_SoC_n).round(1)
     d['max_SoC']  = np.nanmax(indi_SoC_n).round(1)
@@ -43,7 +46,7 @@ def soc(df):
 
 #df = pd.DataFrame(data = soc(pd.read_csv('rndao_pulse2.csv')))
 
-data = soc(pd.read_csv(sys.argv[1]))
+data = soc(pd.read_csv(sys.argv[1]), sys.argv[2], sys.argv[3])
 
 #json file needs to include: timestamp, soc
 
