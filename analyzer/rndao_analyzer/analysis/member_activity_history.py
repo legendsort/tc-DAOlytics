8295feeb4b55f4e50397e35aceba0e60bc9c800d
TjitsevdM <48472085+TjitsevdM@users.noreply.github.com>
2023-03-25 12:13:56 -0700

    Scripts for member actitivity types and networks

--- analyzer/rndao_analyzer/analysis/member_activity_history.py
+++ analyzer/rndao_analyzer/analysis/member_activity_history.py
@@ -8,12 +8,14 @@
 
 # # # # # import libraries # # # # #
 
-import json
 import numpy as np
+import networkx as nx
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-import db_query
+from compute_interaction_matrix_discord import compute_interaction_matrix_discord
+from analytics_interactions_script import DB_access
+from assess_engagement import assess_engagement
 
 
 # # # # #
@@ -22,24 +24,39 @@ def member_activity_history(db_name, connection_string, channels, acc_names, dat
     """
     Computes member activity and member interaction network
 
-    :param db_name: (str) - guild id
-    :param connection_string: (str) - connection to db string
-    :param channels: [str] - list of all channel ids
-    :param acc_names: [str] - list of all account names that should be analysed
-    :param date_range: [str] - list of first and last date to be analysed (one output per date)
-    :param window_param: [int] - first entry: window size in days, second entry: step size of sliding window in days.
-        Currently these values will be default values, in the future, the user might be able to set these in the
-        extraction settings page
-    :param act_param: [int] - first entry: minimal number of interactions for active connections, second entry: minimal
-        number of accounts to have at least ac_int_thr interactions with to be active, third entry: minimal number of
-        interactions to be connected, fourth entry: minimal number of accounts to have at least con_int_thr interactions
-        with to be connected. Currently these values will be default values, in the future, the user might be able to
-        set these in the extraction settings page
+    Input
+    db_name: (str) - guild id
+    connection_string: (str) - connection to db string
+    channels: [str] - list of all channel ids that should be analysed
+    acc_names: [str] - list of all account names that should be analysed
+    date_range: [str] - list of first and last date to be analysed (one output per date)
+    window_param: [int] -
+        entry 1: window size in days. default = 7
+        entry 2: step size of sliding window in days. default = 1
+        (Currently these values will be default values, in the future, the user might be able to set these in the
+        extraction settings page)
+    act_param: [int] -
+        entry 1: INT_THR - int : minimum number of interactions to be active. Default = 1
+        entry 2: UW_DEG_THR - int : minimum number of connections to be active. Default = 1
+        entry 3: PAUSED_T_THR - int : time period to remain paused. Default = 1
+        entry 4: CON_T_THR - int : time period to assess consistently active. Default = 4
+        entry 5: CON_O_THR - int : times to be active within CON_T_THR to be consistently active. Default = 3
+        entry 6: EDGE_STR_THR - int : minimum number of interactions for connected. Default = 5
+        entry 7: UW_THR_DEG_THR - int : minimum number of accounts for connected. Default = 5
+        entry 8: VITAL_T_THR - int : time period to assess for vital. Default = 4
+        entry 9: VITAL_O_THR - int : times to be connected within VITAL_T_THR to be vital. Default = 3
+        entry 10: STILL_T_THR - int : time period to assess for still active. Default = 3
+        entry 11: STILL_O_THR - int : times to be active within STILL_T_THR to be still active. Default = 2
+        (Currently these values will be default values, in the future, the user might be able to adjust these)
 
-    :return: [{}] - list with dictionaries containing data per period. The dictionary contains the last date of the
-        period and the accounts that belong to the different activity types (active, joined, connected)
-
-
+    Output
+    network_dict: {datetime:networkx obj} - dictionary with python datetime objects as keys and networkx graph
+        objects as values. The keys reflect the last date of the WINDOW_D day window over which the network was
+        computed. The values contain the computed networks.
+    activity_dict: {str:{str:set}} - dictionary with keys reflecting each member activity type and dictionaries as
+        values. Each nested dictionary contains an index string as key reflecting the number of STEP_D steps have been
+        taken since the first analysis period. The values in the nested dictionary are python sets with account names
+        that belonged to that category in that period. The length of the set reflects the total number.
     """
 
     # make empty results output array
@@ -48,7 +65,7 @@ def member_activity_history(db_name, connection_string, channels, acc_names, dat
     # # # DATABASE SETTINGS # # #
 
     # set up database access
-    db_access = db_query.DB_access(db_name, connection_string)
+    db_access = DB_access(db_name, connection_string)
 
     # specify the features not to be returned
     ignore_features_db = {'date': 0, 'account_name': 0, 'channel': 0, '_id': 0}
@@ -64,6 +81,25 @@ def member_activity_history(db_name, connection_string, channels, acc_names, dat
 
     # determine maximum start time (include last day in date_range)
     last_start = time_diff - relativedelta(days = window_param[0]-1)
+
+    # initiate result dictionary for network graphs
+    network_dict = {}
+
+    # initiate result dictionaries for engagement types
+    all_arrived = {}
+    all_consistent = {}
+    all_vital = {}
+    all_active = {}
+    all_connected = {}
+    all_new_active = {}
+    all_still_active = {}
+    all_paused = {}
+    all_new_disengaged = {}
+    all_disengaged = {}
+    all_unpaused = {}
+    all_returned = {}
+
+    # # # TO BE IMPLEMENTED: check if there is any past data and if so, load this data instead of creating new dicts
 
 
     # # # ACTUAL ANALYSIS # # #
@@ -86,97 +122,86 @@ def member_activity_history(db_name, connection_string, channels, acc_names, dat
         for i in range(len(date_list_w_str)):
             date_list_w_str[i] = date_list_w[i].strftime('%Y-%m-%d')
 
-        # make empty result array with interactions per account
-        int_per_acc = np.zeros(len(acc_names))
-        int_above_act_thresh = np.zeros(len(acc_names))
-        int_above_con_thresh = np.zeros(len(acc_names))
+        # obtain interaction matrix
+        int_mat = compute_interaction_matrix_discord(acc_names, date_list_w_str, channels, db_access)
 
-        # for each account name
-        for i, acc in enumerate(acc_names):
+        # remove interactions with self
+        int_mat[np.diag_indices_from(int_mat)] = 0
 
-            # creating the db query
-            int_query = db_query.create_query_sum_interactions([acc], channels, date_list_w_str)
+        # assess engagement
+        [graph_out, all_arrived, all_consistent, all_vital, all_active, all_connected, all_paused, all_new_disengaged,
+         all_disengaged, all_unpaused, all_returned, all_new_active, all_still_active] = assess_engagement(int_mat,
+         w_i, np.asarray(acc_names), act_param, window_param[0], all_arrived, all_consistent, all_vital, all_active,
+         all_connected, all_paused, all_new_disengaged, all_disengaged, all_unpaused, all_returned, all_new_active,
+         all_still_active)
 
-            # initiate cursor for db query
-            cursor = db_access.query_db_find(table='heatmaps', query=int_query, ignore_features=ignore_features_db)
-            cursor_list = list(cursor)
+        # make empty dict for node attributes
+        node_att = {}
 
-            # if cursor contains any data
-            if len(cursor_list) > 0:
+        # store account names in node_att dict
+        for i, node in enumerate(list(graph_out)):
+            node_att[node] = acc_names[i]
 
-                # extract all interactions from db
-                interactions = db_query.sum_interactions_features(cursor_list, cursor_list[0].keys())
-
-                # sum interactions of interest
-                all_interactions = interactions["replier"] + interactions["replied"] + interactions["mentioner"] + \
-                                   interactions["mentioned"] + interactions["reacter"] + interactions["reacted"] + \
-                                   interactions["thr_messages"]
-
-                # store total interactions for account
-                int_per_acc[i] = sum(all_interactions)
-
-
-        # # obtain interaction matrix
-        # compute_interaction_matrix_discord(acc_names, date_list_w_str, channels, db_access)
-        #
-        # # store total accounts that this account has at least act_param[1] interactions with
-        # int_above_act_thresh[i] = 0 # # # TODO
-        #
-        # # store total accounts that this account has at least act_param[3] interactions with # # # TODO
-        # int_above_con_thresh[i] = 0  # # # TODO
-
-
-        # # # STORE ACTIVE ACCOUNTS # # #
-
-        # see which accounts are active
-        act_bool = int_per_acc >= act_param[0]
-        # act_bool = int_above_act_thresh >= act_param[1]
-
-        # see which accounts are connected
-        con_bool = int_above_con_thresh >= act_param[3]
-
-
-        # # # STORE RESULTS IN FINAL DICTIONARY # # #
-
-        # make empty result dictionary
-        activity_dict = {}
+        # assign account names in node_att to node attributes of graph_out
+        nx.set_node_attributes(graph_out, node_att, "acc_name")
 
         # store results in dictionary
-        activity_dict["last_date"] = last_date_w
-        activity_dict["active_acc"] = [acc_names[i] for i in range(len(acc_names)) if act_bool[i]] # updated later
-        activity_dict["joined_acc"] = [] # added later
-        activity_dict["connected_acc"] = [acc_names[i] for i in range(len(acc_names)) if con_bool[i]] # updated later
-
-        data_out.append(activity_dict)
-
-    return data_out
+        network_dict[last_date_w] = graph_out
 
 
+    # # # STORE RESULTS IN FINAL DICTIONARY # # #
+
+    # make empty result dictionary
+    activity_dict = {}
+
+    # store results in dictionary
+    activity_dict["all_arrived"] = all_arrived
+    activity_dict["all_consistent"] = all_consistent
+    activity_dict["all_vital"] = all_vital
+    activity_dict["all_active"] = all_active
+    activity_dict["all_connected"] = all_connected
+    activity_dict["all_paused"] = all_paused
+    activity_dict["all_new_disengaged"] = all_new_disengaged
+    activity_dict["all_disengaged"] = all_disengaged
+    activity_dict["all_unpaused"] = all_unpaused
+    activity_dict["all_returned"] = all_returned
+    activity_dict["all_new_active"] = all_new_active
+    activity_dict["all_still_active"] = all_still_active
+
+    return [network_dict, activity_dict]
 
 
 if __name__ == "__main__":
 
     ## sample entries
-    channels = ["👥together-crew", "👥tc-front-end", "👥tc-back-end", "👥tc-design"]
+    channels = ["968110585264898048", "1083353697746161774"]
     acc_names = ["danielo#2815", "Ashish G#1920", "katerinabc#6667", "Ene SS Rawa#0855", "sepehr#3795", "mehrdad_mms#8600",
                  "MagicPalm#5706", "Tanusree#3121", "SimonSekavcnik#2162", "Behzad#1761", "DenisFox.#1743", "nimatorabiv#2903"]
     date_range = ['23/02/01', '23/03/01'] # example for longer range of dates (use this for first extraction)
     # date_range = ['23/02/01', '23/02/07']  # example for single day (date range covers exactly window_d days, use this for daily update)
 
-    CONNECTION_STRING = "mongodb://tcmongo:UCk8nV8MuF8v1cM@104.248.137.224:27017/?authMechanism=DEFAULT"
+    CONNECTION_STRING = "mongodb://tcmongo:UCk8nV8MuF8v1cM@104.248.137.224:27017/"
     DB_NAME = '915914985140531240'
 
     ## analysis parameters
-    window_d = 7 # window size in days
-    step_d = 1 # step size of sliding window in days
+    WINDOW_D = 7 # window size in days
+    STEP_D = 1 # step size of sliding window in days
 
-    ac_int_thr = 1 # minimal number of interactions for active connections
-    ac_acc_thr = 1 # minimal number of accounts to have at least ac_int_thr interactions with to be active
-    con_int_thr = 5 # minimal number of interactions to be connected connection
-    con_acc_thr = 5 # minimal number of accounts to have at least con_int_thr interactions with to be connected
+    INT_THR = 1  # minimum number of interactions to be active					    Default = 1
+    UW_DEG_THR = 1  # minimum number of accounts interacted with to be active	    Default = 1
+    PAUSED_T_THR = 1  # time period to remain paused								Default = 1
+    CON_T_THR = 4  # time period to be consistent active							Default = 4
+    CON_O_THR = 3  # time period to be consistent active							Default = 3
+    EDGE_STR_THR = 5  # minimum number of interactions for connected				Default = 5
+    UW_THR_DEG_THR = 5  # minimum number of accounts for connected				    Default = 5
+    VITAL_T_THR = 4  # time period to assess for vital							    Default = 4
+    VITAL_O_THR = 3  # times to be connected within VITAL_T_THR to be vital		    Default = 3
+    STILL_T_THR = 3  # time period to assess for still active					    Default = 3
+    STILL_O_THR = 2  # times to be active within STILL_T_THR to be still active	    Default = 2
 
     ## call function
-    data_out = member_activity_history(DB_NAME, CONNECTION_STRING, channels, acc_names, date_range, [window_d, step_d],
-                                     [ac_int_thr, ac_acc_thr, con_int_thr, con_acc_thr])
+    data_out = member_activity_history(DB_NAME, CONNECTION_STRING, channels, acc_names, date_range, [WINDOW_D, STEP_D],
+                                     [INT_THR, UW_DEG_THR, PAUSED_T_THR, CON_T_THR, CON_O_THR, EDGE_STR_THR, UW_THR_DEG_THR,
+                                      VITAL_T_THR, VITAL_O_THR, STILL_T_THR, STILL_O_THR])
 
     print(data_out)
